94880b901048b8afdbe2efe9da09ab3c377f4551
Sirisha-Muddana <37927730+Sirisha-Muddana@users.noreply.github.com>
2023-05-24 22:01:06 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "Terraform-task"
+  name = "Terraform1-task"
   instance_type          = "t2.micro"
   key_name               = "cicd"
 
